e803a5a6b393d82e5d3445862181a58a2d265efa
JeanneLee57 <jinhwa.lee.57@gmail.com>
2025-08-13 22:32:24 +0900

    Fix: build error

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,29 @@
 /// <reference types="vite/client" />
+
+declare module '@react-three/fiber' {
+  interface ThreeElements {
+    group: any;
+    mesh: any;
+    primitive: any;
+    directionalLight: any;
+    ambientLight: any;
+    planeGeometry: any;
+    boxGeometry: any;
+    meshStandardMaterial: any;
+  }
+}
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      group: any;
+      mesh: any;
+      primitive: any;
+      directionalLight: any;
+      ambientLight: any;
+      planeGeometry: any;
+      boxGeometry: any;
+      meshStandardMaterial: any;
+    }
+  }
+}
